7732959e7e742903754e105fcb1877ff6e1d15ce
Miraziz.Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2024-01-06 18:02:22 +0500

    Initial lesson project

---
+++ Lesson01/Lesson01/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lesson01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
